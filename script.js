01f333b7840912bcd963e895eacc73f135467bc6
Yusuke0620 <boston19851229@gmail.com>
2024-02-28 12:08:34 +0900

    finish

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+let unko = "Hello World!"
 
+const bigUnko = "He..Hell...Hello World!";
+
+let inoki = ['いーち', `にーい`, 'さーん', 'だー'];
+
+
+// let number = 0;
+// while (number < inoki/length) {
+//     console.log(inoki[number]);
+//     number++
+// }
+
+//if / else
+
+//関数
+const test = (arg) => {
+    if(inoki.length > arg) {
+        console.log('ボンバイエ！');
+    } else {
+        console.log('ボンバ。。');
+    }
+};
+
+
+//オブジェクト
+const unko2 = {
+    color: 'pink',
+    size: 'large',
+    purfume: 'mint',
+    goToToilet: () => {
+        console.log("Hello world!");
+    }
+};
+
+document.getElementsByTagName('button')[0].addEventListener('click', () => {
+    window.alert('test');
+})
+ 
